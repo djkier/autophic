d1427b4d45a50d5cffba770a7955f0335f2d57cb
djkier <djjustinekier@gmail.com>
2025-07-17 22:52:45 +0800

    Add a general method of adding all information

--- src/main/java/org/automation/informationcontroller/Controller.java
+++ src/main/java/org/automation/informationcontroller/Controller.java
@@ -1,5 +1,7 @@
 package org.automation.informationcontroller;
 
+import org.json.JSONObject;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
@@ -13,6 +15,84 @@ public class Controller {
         this.member = new MemberInfo();
         this.patient = new PatientInfo();
         this.baby = new BabyInfo();
+    }
+
+    public void setInformation(String[] args) {
+        //expected value of args
+        //args[0] = json of member information
+        //args[1] = json of mother information
+        //args[2] = json of baby information
+        //args[3] = true or false
+        //args[4] = true of false
+
+        JSONObject member = new JSONObject(args[0]);
+        JSONObject mother = new JSONObject(args[1]);
+        JSONObject baby = new JSONObject(args[2]);
+
+        setMemberInfo(
+                member.getString("memberShipType"),
+                member.getString("memberId"),
+                member.getString("memberFirstName"),
+                member.getString("memberMiddleName"),
+                member.getString("memberLastName"),
+                member.getString("memberSuffix"),
+                member.getString("memberBirthDate"),
+                member.getString("memberGender"),
+                member.getString("memberAddress"),
+                member.getString("memberZip"),
+                member.getString("memberMobileNo"),
+                member.getString("memberEmployeeNo"),
+                member.getString("memberEmployeeName")
+        );
+
+        //args[3] == File mother claim if true
+        if (Boolean.parseBoolean(args[3])) {
+            ArrayList<LocalDate> checkupDates = new ArrayList<>();
+            checkupDates.add(LocalDate.parse(mother.getString("motherCheckUp1")));
+            checkupDates.add(LocalDate.parse(mother.getString("motherCheckUp2")));
+            checkupDates.add(LocalDate.parse(mother.getString("motherCheckUp3")));
+            checkupDates.add(LocalDate.parse(mother.getString("motherCheckUp4")));
+
+            setPatientInfo(
+                    LocalDate.parse(mother.getString("admissionDay")),
+                    LocalDate.parse(mother.getString("dischargeDay")),
+                    LocalTime.parse(mother.getString("admissionTime")),
+                    LocalTime.parse(mother.getString("dischargeTime")),
+                    mother.getString("motherPatientId"),
+                    mother.getString("motherFirstName"),
+                    mother.getString("motherMiddleName"),
+                    mother.getString("motherLastName"),
+                    mother.getString("motherSuffix"),
+                    mother.getString("motherBirthDate"),
+                    mother.getString("motherGender"),
+                    checkupDates,
+                    Integer.parseInt(mother.getString("gravida")),
+                    Integer.parseInt(mother.getString("para")),
+                    mother.getString("tpal"),
+                    mother.getString("aog"),
+                    mother.getString("babyGender"),
+                    mother.getString("accreNumber")
+            );
+        }
+
+        //args[4] == file baby claim if true
+        if (Boolean.parseBoolean(args[4])) {
+            setBabyInfo(
+                    LocalDate.parse(baby.getString("admissionDay")),
+                    LocalDate.parse(baby.getString("dischargeDay")),
+                    LocalTime.parse(baby.getString("admissionTime")),
+                    LocalTime.parse(baby.getString("dischargeTime")),
+                    baby.getString("firstName"),
+                    baby.getString("middleName"),
+                    baby.getString("lastName"),
+                    baby.getString("suffix"),
+                    baby.getString("gender"),
+                    Integer.parseInt(baby.getString("weight")),
+                    baby.getString("nbs"),
+                    baby.getString("accreditationNo")
+            );
+        }
+
     }
 
     public void setMemberInfo(String membershipType,
